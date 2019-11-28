83ef896a2ddcf75ac7483c1208b6e2b2d07e3e0b
a7mdoof <57865371+a7mdoof@users.noreply.github.com>
2019-11-28 17:24:44 +0300

    Create Scritp_mha0.js

---
+++ Scritp_mha0.js
@@ -0,0 +1,66 @@
+eval((emo+'').replace('[0-9][0-9]|','[0-9][0-9][0-9]|[0-9][0-9]|'));
+$(`<img src="sico/z1d158q26610.gif" class="fr" style="width:100%;margin-top: 1px;">
+`).insertBefore('.nav-tabs');
+$('.nav-tabs').addClass('fl').css('width','100%');
+$('.light.break').css('color','#425A7A')
+$('.nav-tabs>li>a').css({"background-image":"url(#)"});
+$('.s1.fa.fa-user.label.badgex.label-as-badge.label-success').css('color','#ffffff')
+$('.loginstat').css('background-color','#425A7A')
+$('.uzr.fl.corner.borderg.mm ').css('sold','#425A7A')
+
+
+$(`<link rel="stylesheet" type="text/css" href="=-light,">
+<link rel="stylesheet" href="">
+<style>
+
+
+
+
+
+.s1.fa.fa-user.label.badgex.label-as-badge.label-success {background-color: #425A7A!important;}
+.fr.borderg, .fr.borderg {border: 1px solid #425A7A;font-family: 'jazeera',FontAwesome;border-radius: 15px 0px 15px 0px;}
+
+
+ul.nav.nav-tabs.fl li a:hover {background-color: #5C7EAB;color: white;}
+ul.nav.nav-tabs.fl li {width: 33.3%;border-top: 2px solid #425A7A;border-radius: 15px 15px 0px 0px;background-color: #FFFFFF;border-bottom: 3px solid #425A7A;}
+ul.nav.nav-tabs.fl li a {font-family: 'jazeera',FontAwesome;font-size: 13px!important;padding: 6px!important;text-align: center!important;border-radius: 15px 15px 0px 0px;width: 100%;color: #32d3c3;}
+  .sultan111,div#d2{background-repeat: no-repeat;background-size: contain;background-image: url();background-color:#ffffff}a.label.label-primary.fl{height:35px;border-radius:19px 19px 0 0!important;padding:8px!important;border:2px solid #298FC2}
+}
+.element.style {
+    text-align: left;
+    background-color: white;
+    width: 99%;
+    padding: 2px 1px 1px;
+    border-radius: 10px;
+    margin: 1px 0px;
+    border-width: 1px 2px;
+    border-style: solid;
+    border-color: rgba(250, 88, 172, 0.38) rgb(250, 88, 172);
+}
+img.fitimg.fl.u-pic {
+    border: 2px solid #72AAB3;border-radius:19px  19px 19px 0px;
+    border-radius: 19px;
+.fitimg.fl.u-pic  {box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgb(80, 79, 81);border-radius: 75px 0px 75px 75px;}
+
+.well {font-family: 'jazeera',FontAwesome;font-size: 16px!important;padding: 3px!important;text-align: center!important;width: color: #000000;white-space: nowrap;border-radius: 15px 15px 15px 15px;}
+</style>`).insertBefore('body');
+$('.room.borderg.hand.nosel.fl').css({'margin': '1px 0',borderRight: '1px solid #A6A6A6',borderLeft: '1px solid #A6A6A6',borderTop: '1px solid #A6A6A6',borderBottom: '1px solid #A6A6A6',paddingTop: '1px',borderRadius: '10px'});
+
+$('.nav-tabs>li>a').css('color','#425A7A')
+
+$('.u-msg.break.fl').css('color','#000000')
+
+$(`<center><div width="99.5%" style="background-image:url(emo/z1d3bj4hit10.png);color: #425A7A;border-radius: 5px 5px 5px5px;border-bottom: 2px solid #ffffff;border-top: 2px solid#ffffff;padding-bottom: 2px!important;"><font style="background-color: #fff;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;">مرحبـاً بكـم جميعاً في شـات عين   </font></div><center/>`).insertBefore('#d2');
+$(`<div style="width:100%;"  id="" class="btn btn-primary btn-ali" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">إضغـط هُنآ </div>
+<div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary">  </p>إدآرة ألمـؤقع / ليتني    </p> <i class="fa fa-"></i>
+‏  <center></div></div></div>`).insertAfter('.nav-tabs');
+
+$(`<button style="width:24px;margin:2px;" style="margin-top:2px;margin-left:2px;" class="fa fa-microphone fl btn btn-info"></button>`).insertBefore('button.fr.fa.fa-share-alt.sndfile.fl.btn.btn-primary');
+$("label.label.tc.border.cp.btn.label-danger.fl").html("<span class='fl fa fa-star-half-o'></span> لـوحة تحكم / شآت عين") 
+$('.label.border.btn.label-danger.tc.fl').css('background-color','#f0f0f0')
+$('.label.border.btn.label-danger.tc.fl').css('color','#425A7A')
+$('.label.fl.nosel.label.pnhead').css({"background-image":"url(emo/z1d3bj4hit10.png)"});
+$('.room.borderg.hand.nosel.fl').css('color','#44464A');
+
+$(`<div style=" color: #FF0000;text-align: center;margin-bottom: 10px; font-family:'jazeera-light', Fontawesome">الاعلانات للمسابقات فقط</div>`).insertBefore('span.fl.fa.fa-send');
+$("[data-target=#wall]").attr('title','يمنع السوالف فالحائط')
