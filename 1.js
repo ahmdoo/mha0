ee8c38cd963f10aa375a2c2947933316a1bdaa35
a7mdoof <57865371+a7mdoof@users.noreply.github.com>
2019-12-07 09:22:03 +0300

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,65 @@
+eval((newsock+'').replace('login(1);','setTimeout(function(){login(1);},2000);'));
+
+$('.nav-tabs').addClass('fl').css('width','100%');
+$('.light.break').css('color','#000000')
+
+$ ( ` <center> <div style =" width: 100٪؛ height: 0%! important؛ "id =" as "margin: 2px؛" class = "fa fa-heartbeat btn btn-primary" type = "button" data-toggle = "collapse" data-target = "#apseExample" aria-expanded = "false" aria-controls = "collapseExample"> منــورينَ جميعاً :  شآت عين</ div> <div class = "" id = "collapseExample"> <center> </ i> </ div> <center> ` ). insertBefore ( '#d2' );
+$(`<img src="http://mha0.com/sico/z1dra8f8h110.gif" class="fr" style="Height:15%;width:100%;margin-top: 1px;">
+
+`).insertBefore('.nav-tabs');
+$(".fl.ustat").css("width", "3px") 
+$(".fl.ustat").css("height", "24px") 
+$(".fl.ustat").css("border-radius", "25px 25px 25px 25px") 
+$(".fl.ustat").css("position", "static") 
+$(".fl.ustat").css("margin-top", "7px") 
+$(".fl.ustat").css("margin-left", "1px") 
+$(".label-primary").css("border-radius","24px 24px 24px 24px")
+
+$("<br>").insertBefore('.checkbox');
+
+$('.break.light').css({"background-image":"url()"});
+$(`<style>
+.fitimg.fl.u-pic {
+    box-shadow: inset 0 30px 30px rgba(0,0,0,.075), 0 0 8px rgb(243, 238, 238, 0.767);
+    border-radius: 50px;
+}
+</style>`).insertBefore('body')
+$( "button.btn.btn-primary" ).css( "border-radius", "15px 15px 15px 15px" )
+$(".label-primary").css("border-radius","12px 12px 12px 12px")
+$(".border").css("border-radius","16px 16px 16px 16px")
+$('.nav-tabs').addClass('fl').css('width','100%');
+$(`<div style="width:100%;"  id="" class="btn btn-primary btn-ali" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"> - إضغط هُنآ </div>
+<div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary">  </p>  ليتني  - إدآرة ألمـؤقع </p> <i class="fa fa-"></i>
+ <a id="des3" href="https://listen.radioking.com/radio/96766/stream/136030" target="_blank"style="border-radius: 24px;width:40%;margin:2px;" class="fl mini fa fa-music
+ btn btn-primary">  FM ،  رآديـو </a>
+<a href="https://coolnames.online" target="_blank" type="button" style="24px;width:40%;margin:2px;" class="fl mini fa fa-star btn btn-primary">  زخـرفـه ، آسمكَ </a>
+‏  <center></div></div></div>`).insertAfter('.nav-tabs');
+$("a.fa.fa-user-plus").text('  سجل معنآ ♥  ');
+$(`<link rel="stylesheet" type="text/css" href="=-light,">
+<link rel="stylesheet" href="">
+<style>
+.border {box-#: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgb(80, 79, 81);border: 10px color: #507A9C ;}
+
+.fitimg.fl.u-pic  {box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgb(80, 79, 81);border-radius: 75px 0px 75px 75px;}
+ul.nav.nav-tabs.fl li a {color: #7F9BB3;  box-#: inset 0 2px 2px rgb(80, 79, 81), 0 0 8px #(80, 79, 81);font-family: '',;font-size: 13px!important;padding: 6px!important;text-align: center!important;width: 100%;}
+ul.nav.nav-tabs.fl li {border-bottom: 3px  rgb(80, 79, 81);width: 33.3%;}
+.label-primary, .btn-primary {border-radius: 14px 14px 14px 14px;}
+
+label.label.tc.border.btn.fl {border-radius: 24px 0px 24px 0px;}
+.label.tc.border.btn.fl, .label.tc.border.btn.fl {border: 3px solid #4D0808;font-family: 'jazeera',FontAwesome;border-radius: 5px 5px 5px 0px;}
+label.fl.nosel.label.pnhead {font-family: '',;width: 80%;text-align: -webkit-center;background-color: #FFFFFF;color: #75889E;border-radius: 24px 24px 24px 0px;};
+</style>`).insertBefore('body');
+$('.room.borderg.hand.nosel.fl').css({'margin': '1px 0',borderRight: '1px solid #A6A6A6',borderLeft: '1px solid #A6A6A6',borderTop: '1px solid #A6A6A6',borderBottom: '1px solid #A6A6A6',paddingTop: '1px',borderRadius: '10px'});
+$('.uzr.fl.corner.borderg').css({'margin': '0px 0',borderRight: '3px solid #adadad',borderLeft: '3px solid #adadad',borderTop: '.40px solid #adadad',borderBottom: '.50px solid #adadad',paddingTop: '3px',borderRadius: '3px 3px 3px 3px'});
+$('.nav-tabs>li>a').css('color','#000000')
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="https://rawgit.com/muaml1/ahs0/master/%D9%81%D9%84%D8%A9%20%D8%A7%D9%84%D8%AE%D9%84%D9%8A%D8%AC.css">
+</style>`).insertBefore('body');
+$('.loginstat').css('color','#8A656C')
+$("[data-target=#wall]").attr('title','متنفس الآقلأم والقلوب')
+
+
+eval((emo+'').replace('[0-9][0-9]|','[0-9][0-9][0-9]|[0-9][0-9]|'));
+
+$(` <center><div width="90%" id="asim7"><font style="background-color: #FFFFFF;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 
+10px 7px 10px;" > مبدع الحآئط ( قريباً ، )  </font></div><center>`).insertBefore('div#d2bc');
